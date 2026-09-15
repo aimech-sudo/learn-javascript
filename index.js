3c4c644b9a9a3cbf421eaa1179dabe6f52118890
aimech-sudo <dkskzmfhshtm@gmail.com>
2026-09-15 13:51:39 +0900

    Feat : 함수 실습

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ console.log("Hello js")
 let username = "후르츠링"
 console.log("username", username)
 
-username = "강민수";
+username = "강성훈";
+studentname = "강성훈";
 
 console.log("username", username)
 
@@ -25,3 +26,49 @@ console.log("🚀 ~ empty:", empty)
 // 변수 선언만
 let userage;
 console.log("🚀 ~ userage:", userage)
+
+
+/**
+ * 함수: 특정 기능을 수행하도록 만든 코드 조각 
+ */
+// 함수를 정의한다, 선언한다.
+// function greeting() {
+//   //실행할 로직을 중괄호 안에 작성
+//   console.log(`${username}님, 반갑습니다!`)
+//   //console.log(username + "님, 반갑습니다!")
+// }
+
+// 화살표 함수 변환
+const greeting = (studentname, usermbti) => {
+//실행할 로직을 중괄호 안에 작성
+  console.log(`${studentname}님, 반갑습니다!`)
+  console.log(`MBTI는 ${usermbti}이시군요!`)
+};
+
+// 함수를 실행한다, 호출한다
+greeting()
+greeting("강성훈", "ISTJ")
+
+
+// 미션: "프론트엔드부터 만드는 AI챗봇 수강생 ???입니다." 문자열이 콘솔에 찍히는 화살표 함수 만들고 실행하기
+const introduce = () => {
+  console.log(`프로트엔드부터 만드는 AI챗봇 수강생 ${username}입니다. `)
+};
+
+introduce()
+
+// 결과값 반환
+const add = (num1, num2) => {
+return num1 + num2
+};
+
+const doubleSum = (num) => {
+  console.log(num)
+  console.log("hello~~")
+  return num * 2
+};
+
+const sum = add(10, 20)
+console.log("🚀 ~ sum:",sum)
+
+console.log("doubleSum", doubleSum(sum))
