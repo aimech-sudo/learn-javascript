1f0c44c67ba0c48cdb7e07cbde6faeee091e0298
aimech-sudo <dkskzmfhshtm@gmail.com>
2026-09-15 10:52:11 +0900

    데이터타입 확인

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 console.log("Hello js")
 
 // 변수: 값을 저장하는 이름을 가진 공간
-const username = "후르츠링";
+//const username = "후르츠링";
+let username = "후르츠링"
 console.log("username", username)
+
+username = "강민수";
+
+console.log("username", username)
+
+// 데이터 타입
+const num = 10; // 숫자
+console.log("🚀 ~ num:", num, "num type", typeof num)
+
+const text = "안녕하세요" // 문자열
+console.log("🚀 ~ text type:", typeof text)
+
+const isDarkMode = true; // boolean
+console.log("🚀 ~ isDarkMode, type:", typeof isDarkMode)
+
+const empty = null;
+console.log("🚀 ~ empty:", empty)
+
+// 변수 선언만
+let userage;
+console.log("🚀 ~ userage:", userage)
